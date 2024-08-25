4c50b6d09467afe268d1400272f605fbd47ef8f0
Sc0rp113 <149697986+Sc0rp113@users.noreply.github.com>
2024-08-25 09:33:42 +0300

    Add files via upload

---
+++ Homework/Homework Lesson 15 (if calculator)/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Lesson_15__if_calculator_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {while(true) 
+            {
+                double a, b;
+                string operation;
+                Console.WriteLine("Введите первое число ");
+                bool suc1 = double.TryParse(Console.ReadLine(), out a);
+                try
+                {
+                    Console.WriteLine("Введите первое число ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите второе число ");
+                    b = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введено неверное значение!");
+                    Console.ReadLine();
+                    continue;
+                }
+                Console.WriteLine("Введите символ операции ");
+                operation = Console.ReadLine();
+                if (operation == "+")
+                {
+                    Console.WriteLine(a + "+" + b + "=" + (a + b));
+                }
+                else if (operation == "-")
+                {
+                    Console.WriteLine(a + "-" + b + "=" + (a - b));
+                }
+                else if (operation == "*")
+                {
+                    Console.WriteLine(a + "*" + b + "=" + (a * b));
+                }
+                else if ((operation == "/") || (operation == ":"))
+                {
+                    switch (b)
+                    {
+                        case 0:
+                            Console.WriteLine("Деление на 0 запрещено!");
+                            break;
+                        default:
+                            Console.WriteLine(a + ":" + b + "=" + (a - b));
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Введен некорректный оператор - " + operation);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Homework/Homework Lesson 17/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int count, count2, limit, sumodd = 0, sumeven = 0;
+                uint oddMumbersCount = 0, evenMumbersCount = 0;
+                try
+                {
+                    Console.WriteLine("Укажите диапазон чисел.");
+                    Console.WriteLine("Нижняя граница ");                    
+                    count2 = count = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Верхняя граница ");
+                    limit = int.Parse(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введён некорректный диапозон чисел");
+                    Console.WriteLine("Нажмите Enter чтобы продолжить");
+                    Console.ReadLine();
+                    continue;
+                }
+                if (count <= limit)
+                {
+                    while (count <= limit)
+                    {
+                        if (count % 2 == 0)
+                        {
+                            oddMumbersCount++;
+                            sumodd = sumodd + count;
+                        }
+                        else
+                        {
+                            evenMumbersCount++;
+                            sumeven = sumeven + count;
+                        }
+                        count++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Нижняя граница диапазона не может быть больше верхней.");
+                    Console.WriteLine("Нажмите Enter чтобы продолжить");
+                    Console.ReadLine();
+                    continue;
+                }
+                Console.WriteLine("В диапазоне чисел [" + count2 + ";" + limit + "] находится " + oddMumbersCount + " чётных и " + evenMumbersCount + " нечётных чисел");
+                Console.WriteLine("Сумма всех чётных чисел = "+ sumodd);
+                Console.WriteLine("Сумма всех нечётных чисел = "+ sumeven);
+                Console.WriteLine("Сумма чётных и нечётных чисел = " + (sumodd + sumeven));
+            }
+        } 
+    }
+}
